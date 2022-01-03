161b9e3ddc11f8adb750730945cb19aaaf0ce77b
zael-mab <zackliwa@gmail.com>
2022-01-03 13:42:27 +0100

    working on regex parser

--- computor.js
+++ computor.js
@@ -5,15 +5,21 @@
 // solve quation S={( -b+√Delta / 2a ); (-b-√Delta / 2a)}
 
 // set a structure
+
+//    /^X[\^]?[0-2]{1}$|^X$/
+//    X[\^]?[0-2]{0,}
+
+
 const struct = {
-    degree: 0,
     firstArray:{
+        degree: 0,
         arr:'',
         x0:'',
         x1:'',
         x2:''
     },
     secondArray:{
+        degree: 0,
         arr: '',
         x0:'',
         x1:'',
@@ -53,7 +59,9 @@ console.log (struct);
 /////////////semplify and set Data
 const setData = (str)=>{
     let j = 0;
+    const reg = /X$|X\^[0-2]$/;
     for (let i = 0; i <= str.arr.length; i++){
+
         if (str.arr[i] === 'X' && str.arr[i + 1] === '^'){
             // 
             if (str.arr[i + 2] > '2'){
@@ -69,18 +77,32 @@ const setData = (str)=>{
                 str.arr = str.arr.replace('X^1', 'X');
             }
         }
+// /////////////
+        if (str.arr[i] === 'X' && str.arr[i + 1] !== '^' && Number.isInteger(parseInt(str.arr[i + 1]))){
+            console.log(`Error ^ is missing between ${str.arr[i]} ${str.arr[i + 1]}`);
+            break;
+        }
+// ///////////
+        // if (){
+
+        // }
+// ////////////
         if (str.arr[i] === '+' || str.arr[i] === '-' || i  === str.arr.length){
 
             if (i == 0)
                 continue;
             const tmp = str.arr.slice(j, i);
-            console.log(tmp,'|', i, j);
-            // if (!isNaN(parseInt(tmp))){
-            //     console.log(`====>${(parseInt(tmp))}`);
-            //     num.push(parseInt(tmp));
-            // }else {
-            //     num.push(tmp);
-            // }
+            let d = degree(tmp);
+            if (d == 0){
+                str.x0 = tmp.slice();
+            }else if (d == 1){
+                str.x1 = tmp.slice();
+            }else if (d == 2){
+                str.x2 = tmp.slice();
+            }
+            str.degree = d > str.degree ? d : str.degree;
+            
+            console.log(tmp,'|', i, j, `D = ${d}`);
             j = i;
         }
         // 
@@ -88,45 +110,21 @@ const setData = (str)=>{
     return str;
 }
 
+// ////////////////////////
+const degree = (str) => {
+    for(let i = 0; i < str.length; i++){
+        if (str[i] === 'X' && str[i + 1] === '^' && Number.isInteger(parseInt(str[i + 2]))){
+            return parseInt(str[i + 2]);
+        }
+        if (str[i] === 'X' && str[i + 1] !== '^'){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+// ////////////////////////////
 struct.firstArray = setData(struct.firstArray);
 struct.secondArray = setData(struct.secondArray);
 console.log (struct);
-// /////////////////
-
-
-
-
-// const simplify = (str) => {
-//     console.log (`\n${str}`);
-//     let num = [];
-//     let j = 0;
-//     for (let i = 0; i <= str.length; i++){
-//         if (str[i] === '+' || str[i] === '-' || i  === str.length){
-
-//             if (i == 0)
-//                 continue;
-            
-//             let nb;
-//             const tmp = str.slice(j, i);
-//             console.log(tmp,'|', i, j);
-
-//             if (!isNaN(parseInt(tmp))){
-//                 console.log(`====>${(parseInt(tmp))}`);
-//                 num.push(parseInt(tmp));
-//             }else {
-//                 num.push(tmp);
-//             }
-//             j = i;
-//         }
-//     }
-//     console.log (`num = ${num}\n`);
-//     return num;
-// }
-
-// const firstArray = simplify(f[0]);
-// console.log(firstArray.length);
-// for(let i = 0; i < firstArray.length; i++){
-//     console.log(Number.isInteger(firstArray[i]));
-
-// }
-// const secondArray = simplify(f[1]);
