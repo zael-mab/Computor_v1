6fac9e3d295a5b62d0484de5d46bd0400edbe0b9
zael-mab <zackliwa@gmail.com>
2022-01-10 13:34:11 +0100

    fix the shity code

--- computor.js
+++ computor.js
@@ -11,38 +11,38 @@
 
 
 const struct = {
-    firstArray:{
-        degree: 0,
-        arr:'',
-        par:[],
-        x0:[],
-        x1:[],
-        x2:[]
-    },
-    secondArray:{
+    firstArray: {
         degree: 0,
         arr: '',
-        par:[],
-        x0:[],
-        x1:[],
-        x2:[]
+        par: [],
+        x0: [],
+        x1: [],
+        x2: []
+    },
+    secondArray: {
+        degree: 0,
+        arr: '',
+        par: [],
+        x0: [],
+        x1: [],
+        x2: []
     }
 };
 
 
 // check if the arguments
 var arr = [];
-if (process.argv.length > 3){
-    console.log (`Error to many args...`);
+if (process.argv.length > 3) {
+    console.log(`Error to many args...`);
     process.exit(1);
-}else if (process.argv.length == 2){
-    console.log ('Error, please add an argument');
+} else if (process.argv.length == 2) {
+    console.log('Error, please add an argument');
     process.exit(1);
 }
 
 
 //////////// get the equation from the argument
-for (let i = 2; i < process.argv.length; i++){
+for (let i = 2; i < process.argv.length; i++) {
     arr.push(process.argv[i]);
 }
 
@@ -50,88 +50,182 @@ for (let i = 2; i < process.argv.length; i++){
 ///////////////// splite the equation
 const f = arr[0].split('=');
 
-if (f.length == 2){
+if (f.length == 2) {
     struct.firstArray.arr = f[0].trim();
     struct.secondArray.arr = f[1].trim();
-}else if (f.length == 1){
+} else if (f.length == 1) {
     struct.firstArray.arr = f[0].trim();
     struct.secondArray.arr = '0';
 }
 
 /////////////////// match
 
+// extract Xˆe from string
 const regex1 = /X[\^]?[0-2]{0,}/gi;
+// check Xˆe format
 const regex2 = /^X[\^][0-2]{1}$|^X$/i;
-const regex3 = /^[^a-wy-z<>%@#&!_.]+$/ig;
+// check sting 
+const regex3 = /^[^a-wy-z<>%@#&!_,]+$/ig;
 
-if (arr[0].match(regex3) === null){
-    console.log (`Syntax Error`);
-    process.exit (1);
+if (arr[0].match(regex3) === null) {
+    console.log(`Syntax Error`);
+    process.exit(1);
 }
 
-const parse = require ('./parse');
+const parse = require('./parse');
 
+// 
 parse.checkSyntax(regex1, regex2, arr[0]);
 
 struct.firstArray = parse.setData(struct.firstArray);
 struct.secondArray = parse.setData(struct.secondArray);
 
-console.log (struct);
-//  ([\+\-]{0,}[0-9]{1,})([\*\/]{1}[0-9]{1,})
+struct.firstArray.arr =
+    console.log(struct);
+
+// +-n*/m+-
+const reg = /[\+\-]{0,1}\s{0,}[0-9]{1,}\s{0,}[\*\/]\s{0,}[0-9]{1,}/;
+// +-n-+
+const regg = /[\+\-]{1}[0-9]{1,}[\+\-]{0,}(?![\/\*])|^[0-9]{1,}[\+\-]{1}|(?![\/\*])[0-9]{1,}$/;
+
+// (+-X/*Y)
+// [^\^\/\*][\+-]{0}[0-9]{1,}[\/\*]{1}[0-9]{1,}
+// +-X+-Y-+
+// [\+\-]{1}[0-9]{1,}[\+\-]{1,}(?![\/\*])[0-9]{1,}[\+\-]{1}|[\+\-]{1}[0-9]{1,}[\+\-]{1,}(?![\/\*])[0-9]{1,}$|^[\+\-]{0,}[0-9]{1,}[\+\-]{1,}(?![\/\*])[0-9]{1,}[\+\-]{1}
+// 
 
 
+
+//  //////////////
 const produceForm = (arr, degree) => {
     let holder;
     const cal = {
-        index:0,
+        index: 0,
         x: undefined,
-        sign: undefined,
+        xSign: undefined,
+        oper: undefined,
         y: undefined,
+        ySign: undefined,
         total: 0
     };
+    let total = 0;
 
-    
     arr.forEach(element => {
-        holder = element.slice();
-        let match1 = holder.match(/[\*\/]/g);
-        
-        // / or * operation
-        if (holder.match(/[\*\/]/g)){
-            console.log (`\t [${holder}]operation ${match1}`);
-            match1 = holder.match(/(?:[\*\/])[\+-]?[0-9]{1,}/g);
-            console.log (`\t\tsec match =>${match1}`);
-            match1 = holder.match(/^[\+\-]{0,}[0-9]{1,}/);
-            console.log (`\t ther match =>${match1}`);
-            // console.log (`\t four match =>${match1}`);
+        holder = element.slice().replace(/\s/g, '');
+        const numbers = holder.match(/[0-9]{1,}/g);
+        const s = holder.match(/\*\s{0,}\/|\/\s{0,}\*/g);
+
+        cal.index = 0;
+        cal.x = undefined;
+        cal.y = undefined;
+        cal.xSign = undefined;
+        cal.ySign = undefined;
+        cal.total = 0;
+        for (let j = 0; j < holder.length; j++) {
+
+            // looking for the sign
+            if (holder[j].match(/[\+\-]/)) {
+                cal.index = 0;
+                cal.ySign = (cal.ySign === undefined && cal.x) ? holder[j] : cal.ySign;
+                cal.xSign = (!cal.x && !cal.y) ? holder[j] : cal.xSign;
+            }
+
+            // Looking for the operation
+            if (holder[j].match(/([\*\/])/)) {
+                cal.index = 0;
+                cal.oper = holder[j];
+                // console.log('sign =' + cal.oper);
+            }
+            // Looking for a Number
+
+            if (holder[j].match(/[0-9]/) && cal.index == 0) {
+                cal.y = (cal.y === undefined && cal.x) ? parseFloat(holder.slice(j, )) : cal.y;
+                cal.x = (cal.x) ? cal.x : parseFloat(holder.slice(j, ));
+                cal.index = 1;
+
+                console.log('x=' + cal.x + ' ' + 'y=' + cal.y + ' | ' + holder.slice(j, ));
+            }
+            //  ////////////////////////////
+            if (cal.x && cal.y) {
+                // console.log(`XSIGN=${cal.xSign}`);
+                let factor = 1;
+                factor = cal.xSign === '-' ? -1 : 1;
+                cal.x = calculate(cal.x, factor, '*');
+                factor = cal.ySign === '-' ? -1 : 1;
+                cal.y = calculate(cal.y, factor, '*');
+                if (cal.oper != undefined) {
+                    cal.total += calculate(cal.x, cal.y, cal.oper);
+                    console.log(`$-------->x=${cal.x} ${cal.oper} y=${cal.y} = ${cal.total}`);
+                    cal.x = undefined;
+                    cal.y = undefined;
+                    cal.xSign = undefined;
+                    cal.ySign = undefined;
+                    cal.oper = undefined;
+                } else {
+                    cal.total += calculate(cal.x, cal.y, '+');
+                    cal.x = undefined;
+                    cal.y = undefined;
+                    console.log(`$-------->x=${cal.x} + y=${cal.y} = ${cal.total}`);
+                }
+            }
+            // console.log(`XSIGN=${cal.oper}`);
+            if (cal.x && cal.oper && cal.total != 0) {
+                cal.x = calculate(cal.x, (cal.xSign === '-' ? -1 : 1), '*');
+                var t = cal.total;
+                cal.total = calculate(cal.total, cal.x, cal.oper);
+                console.log(`$---->total=${t} ${cal.oper} x=${cal.x} = ${cal.total}`);
+                cal.oper = undefined;
+                cal.x = undefined;
+                cal.xSign = undefined;
+            }
+            // if (cal.x && cal.oper && cal.total > 0) {
+            //     cal.x = calculate(cal.x, (cal.xSign === '-' ? -1 : 1), '*');
+            //     cal.total = calculate(cal.total, cal.x, cal.oper);
+            //     console.log(`$---->total=${cal.total} ${cal.oper} x=${cal.x} = ${cal.total}`);
+            //     cal.oper = undefined;
+            //     cal.x = undefined;
+            //     cal.xSign = undefined;
+            // } // console.log('Y');
+
         }
-        console.log (`holder === ${holder} | ${parseInt(holder)}`);
-
+        console.log('------------');
+        if (cal.x && !cal.y && !cal.oper) {
+            cal.x = calculate(cal.x, (cal.xSign === '-' ? -1 : 1), '*');
+            console.log(' x = ' + cal.x);
+            cal.total += cal.x;
+            cal.xSign = undefined;
+            cal.x = undefined;
+        }
+        total += cal.total;
     });
-
-
-    console.log (cal);
+    cal.total = total;
+    console.log(cal);
 }
 
 
+
 const calculate = (x, y, sign) => {
-    if ((x === 0 || y === 0)&& sign === '/'){
+    if (!x | !y) {
+        console.log('E');
+        process.exit(1);
+    }
+    if ((x === 0 || y === 0) && sign === '/') {
         console.log('Error devision by 0');
         process.exit(1);
     }
     switch (sign) {
         case '+':
-             return x + y;
+            return x + y;
         case '/':
-            return  x / y;
+            return x / y;
         case '-':
-            return  x - y;
+            return x - y;
         case '*':
-            return  x * y;
+            return x * y;
     }
 };
 
 
 
 
-produceForm (struct.firstArray.x0, 0);
-
+produceForm(struct.firstArray.x0, 0);
--- parse.js
+++ parse.js
@@ -1,17 +1,15 @@
-
-
 const checkSyntax = (regex1, regex2, arr) => {
 
     const matches = arr.match(regex1);
     // console.log (matches);
-    
-    if (matches != null){
+
+    if (matches != null) {
         matches.forEach(element => {
             // console.log(`element => ${element}`);
             const check = element.match(regex2);
-            
-            if (check === null){
-                console.log (`syntax Error [${element}] | ${arr}`);
+
+            if (check === null) {
+                console.log(`syntax Error [${element}] | ${arr}`);
                 process.exit(1);
             }
         });
@@ -24,36 +22,36 @@ const setData = (str) => {
     const regex = /\*\s{0,}\/|\/\s{0,}\*|\/\s{0,}\/|\*\s{0,}\*/;
 
     str.arr = str.arr.replace(/\s/g, '');
-    if (str.arr.match(regex) || str.arr.match(/[\+\-][\/\*]/g)){
-        console.log ('Syntax Error');
+    if (str.arr.match(regex) || str.arr.match(/[\+\-][\/\*]/g)) {
+        console.log('Syntax Error');
         process.exit(1);
     }
 
-    for (let i = 0; i <= str.arr.length; i++){
+    for (let i = 0; i <= str.arr.length; i++) {
 
-// ///////////////
+        // ///////////////
         parentheses = str.arr[i] === '(' ? parentheses + 1 : parentheses;
         parentheses = str.arr[i] === ')' ? parentheses - 1 : parentheses;
 
-// ///////////////
-        if ((str.arr[i] === 'X' || str.arr[i] === 'x') && str.arr[i + 1] === '^'){
-            if (str.arr[i + 2] === '0'){
+        // ///////////////
+        if ((str.arr[i] === 'X' || str.arr[i] === 'x') && str.arr[i + 1] === '^') {
+            if (str.arr[i + 2] === '0') {
                 str.arr = str.arr.replace(/X\^0/gi, 1);
             }
             //
-            if (str.arr[i + 2] === '1'){
+            if (str.arr[i + 2] === '1') {
                 str.arr = str.arr.replace(/X\^1/gi, 'X');
             }
-            }
+        }
         // check for multiple minus signs and reduce the form.
-        if (str.arr.match(/(\-\s{0,}\-|\-\-)/g)){
-            str.arr = str.arr.replace(/(\-\s{0,}\-|\-\-)/g , '+');
+        if (str.arr.match(/(\-\s{0,}\-|\-\-)/g)) {
+            str.arr = str.arr.replace(/(\-\s{0,}\-|\-\-)/g, '+');
         }
         // check for multiple minus and plus signs and reduce the form.
-        if (str.arr.match(/(\+\s{0,}\-|\+\-)|(\-\s{0,}\+|\-\+)/g)){
-            str.arr = str.arr.replace(/(\+\s{0,}\-|\+\-)|(\-\s{0,}\+|\-\+)/g , '-');
+        if (str.arr.match(/(\+\s{0,}\-|\+\-)|(\-\s{0,}\+|\-\+)/g)) {
+            str.arr = str.arr.replace(/(\+\s{0,}\-|\+\-)|(\-\s{0,}\+|\-\+)/g, '-');
         }
-        
+
         // ///////////////
         // if (str.arr[i] === '('){
         //     p = i;
@@ -65,20 +63,21 @@ const setData = (str) => {
         //     console.log (`holder\t${holder}`);
         // }
 
-// ////////////////
-        if ((str.arr[i] === '+' || str.arr[i] === '-' || i  === str.arr.length) &&
-         (str.arr[i - 1] !== '/' && str.arr[i - 1] !== '*')){
+        // ////////////////
+        if ((str.arr[i] === '+' || str.arr[i] === '-' || i === str.arr.length) &&
+            (str.arr[i - 1] !== '/' && str.arr[i - 1] !== '*')) {
             let tmp;
 
             if (i == 0)
                 continue;
             tmp = str.arr.slice(j, i).trim();
             let d = degree(tmp);
-            if (d == 0){
+            // str.par.push(tmp);
+            if (d == 0) {
                 str.x0.push(tmp);
-            }else if (d == 1){
+            } else if (d == 1) {
                 str.x1.push(tmp);
-            }else if (d == 2){
+            } else if (d == 2) {
                 str.x2.push(tmp);
             }
             str.degree = d > str.degree ? d : str.degree;
@@ -87,11 +86,11 @@ const setData = (str) => {
         }
 
     }
-    if (parentheses > 0){
+    if (parentheses > 0) {
         console.log(`Error missing parenthese: ')' .`);
         process.exit(1);
-    }else if (parentheses < 0){
-        console.log (`Error missing parenthese: '(' .`);
+    } else if (parentheses < 0) {
+        console.log(`Error missing parenthese: '(' .`);
         process.exit(1);
     }
     return str;
@@ -99,15 +98,15 @@ const setData = (str) => {
 
 
 const degree = (str) => {
-    for(let i = 0; i < str.length; i++){
-        if ((str[i] === `X`|| str[i] === 'x') && str[i + 1] === '^'){
+    for (let i = 0; i < str.length; i++) {
+        if ((str[i] === `X` || str[i] === 'x') && str[i + 1] === '^') {
             return parseInt(str[i + 2]);
         }
-        if ((str[i] === 'X'|| str[i] === 'x') && str[i + 1] !== '^'){
+        if ((str[i] === 'X' || str[i] === 'x') && str[i + 1] !== '^') {
             return 1;
         }
     }
     return 0;
 }
 
-module.exports = {checkSyntax, setData};
+module.exports = { checkSyntax, setData };
